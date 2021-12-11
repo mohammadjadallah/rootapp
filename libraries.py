3c866bb2d8c0d9c4a983daad1cad0497a8b8aba7
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-12-12 00:28:12 +0200

    libraries that used

---
+++ libraries.py
@@ -0,0 +1,11 @@
+    # all libraries I used it in the project
+    
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter.messagebox import showerror, showwarning
+    from tkinter.filedialog import asksaveasfile
+    from collections import deque
+    from tabulate import tabulate
+    from sympy import *
+    from webbrowser import open_new_tab
+    from matplotlib.pyplot import style as st 
